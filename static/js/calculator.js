546fe29f6cef826ed7b725eb9768ff6179116c4a
Grace Chung <gcw.chung@gmail.com>
2019-11-27 13:20:46 -0800

    Implemented currency conversion

--- static/js/calculator.js
+++ static/js/calculator.js
@@ -9,14 +9,30 @@ $(document).ready(function() {
         let gold = parseInt( $("#gold").val() );
         let silver = parseInt( $("#silver").val() );
         let copper = parseInt( $("#copper").val() );
-
+       
         let quantity = parseInt( $("#quantity").val() );
-        let profit = parseInt( $("#profit").val() );
-
+        let profit;
+        
+        if ( $("#gold").val() == "") {
+            gold = 0;
+        }
+        if ( $("#silver").val() == "") {
+            silver = 0;
+        }
+        if ( $("#copper").val() == "") {
+            copper = 0;
+        }
+       
         // convert everything to copper ;_; 
+        const g2c = gold * 10000
+        const s2c = silver * 100
         // then add all currency together
+        const copperTotal = g2c + s2c + copper;
         // then multiply copper value to quantity amount
+        profit = copperTotal * quantity;
+        console.log(profit)
         // set that as total
+    
         // convert that total into gold, silver, copper
 
     }); // end of click() - calculate button
